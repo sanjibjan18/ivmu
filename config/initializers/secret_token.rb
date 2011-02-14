8793e032014331da9c9f94e7c213c2fafbd79361
vijendra <vijendrakarkala@gmail.com>
2011-02-14 17:04:27 +0530

    application base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muvi::Application.config.secret_token = '7883eeb98dc2d662155e975d142e4635ebd8bf7c348ce815a8fa654050022522c8b20e1ed940a58c7e06087710fe444c9197ffb620597baffb01397198a09fe6'
