31b4df8b64f23b7a47bdb72625c52bf42b76deb0
vijendra <vijendrakarkala@gmail.com>
2011-02-17 18:18:56 +0530

    acts_as-commentable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,5 +2,6 @@ class CommentsController < ApplicationController
  def create
    movie = Movie.find(params[:movie_id])
    movie.comments.create(params[:comment])
+   redirect_to root_path
  end
 end
--- app/controllers/fb_tests_controller.rb
+++ app/controllers/fb_tests_controller.rb
@@ -1,8 +1,10 @@
 class FbTestsController < ApplicationController
   def index
     #This is just for demonstration purpose only. To be removed at latter stage. 
-   # client = Mogli::Client.new(current_user.oauth2_token)
+    #client = Mogli::Client.new(current_user.oauth2_token)
     #@myself  = Mogli::User.find("me",client)
+    #Mogli::Post.new(:message => "This is a test message at #{Time.now} from FacebookGraphTestController to a page")
+    #client.post("#...@page.id}/feed", nil, post) 
     #@likes = @myself.likes
     #@friends = @myself.friends
  
