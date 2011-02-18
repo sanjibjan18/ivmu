cd0541a6cfa41706ff3b7e41d82909d04d6a5385
vijendra <vijendrakarkala@gmail.com>
2011-02-18 11:08:24 +0530

    post to wall test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,13 @@ class CommentsController < ApplicationController
    movie.comments.create(params[:comment])
    redirect_to root_path
  end
+ 
+ def post_to_wall
+    comment = Comment.find(params[:id])
+    client = Mogli::Client.new(current_user.oauth2_token)
+    @myself  = Mogli::User.find("me",client)
+    post = Mogli::Post.new(:message => comment.comment)
+    @myself.feed_create(post)
+    redirect_to root_path
+  end
 end
