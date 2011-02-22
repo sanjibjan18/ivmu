23de476dee8e8380a2eb3776cdbc7b462d986595
vijendra <vijendrakarkala@gmail.com>
2011-02-22 12:40:21 +0530

    friends likes

--- app/models/facebook_feed.rb
+++ app/models/facebook_feed.rb
@@ -1,5 +1,5 @@
 class FacebookFeed < ActiveRecord::Base
   belongs_to :user
-  scope :facebook_friend_likes, lambda{|fbid|  where("fbid != ?", fbid)}
+  scope :facebook_friend_likes, lambda{|fbid|  where("fbid <> ?", fbid)}
   scope :like_name, lambda{|name| where("value like ? ", "#{name}%") }
 end
