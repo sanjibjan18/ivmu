0ba56446eef8e8093300d7a6a4e7aa88b70d40b3
vijendra <vijendrakarkala@gmail.com>
2011-02-22 15:40:27 +0530

    fb item id

--- db/migrate/20110222093722_add_fb_item_id_field_to_facebook_feeds.rb
+++ db/migrate/20110222093722_add_fb_item_id_field_to_facebook_feeds.rb
@@ -1,6 +1,5 @@
 class AddFbItemIdFieldToFacebookFeeds < ActiveRecord::Migration
   def self.up
-    remove_column :facebook_feeds, :fb_item_id 
     add_column :facebook_feeds, :fb_item_id, :string
   end
 
