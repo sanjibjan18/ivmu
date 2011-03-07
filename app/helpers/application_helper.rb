2b5c7bef06b1ebc0345a63e967c6c314f17d6396
isiri <isiri@isiri-desktop.(none)>
2011-03-07 15:05:03 +0530

    rating bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def ratingbar(rating)
+    text = ""
+    if rating.to_i >= 50
+      text += "<div class='positive' style='width:#{rating}%;'> &nbsp;</div>"
+    else
+      text += "<div class='negative' style='width:#{rating}%;'> &nbsp; </div>"
+    end
+    text.html_safe
+  end
+
 end
+
