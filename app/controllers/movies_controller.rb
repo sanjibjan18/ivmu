277bf14f26f0f08215fb2e3fd7e4e825a1a5458f
isiri <isiri@isiri-desktop.(none)>
2011-03-07 18:06:22 +0530

    search bar

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,11 @@ class MoviesController < ApplicationController
   skip_before_filter :authenticate_user!
   layout 'website'
 
+  def index
+    @search = Movie.search(params[:search])
+    @movies = @search.all.paginate(:page => params[:page] || 1, :per_page => 10)
+  end
+
   def show
     @movie = Movie.find_using_id(params[:id]).includes(:comments).first
   end
