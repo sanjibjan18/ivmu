6cffe0e3ed47d84f42fbfd8addc9a91d9cc4cdf1
isiri <isiri@isiri-desktop.(none)>
2011-03-07 15:19:13 +0530

    progress bar changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@ module ApplicationHelper
   def ratingbar(rating)
     text = ""
     if rating.to_i >= 50
-      text += "<div class='positive' style='width:#{rating}%;'> &nbsp;</div>"
+      text += "<div class='positive' style='width:#{rating}%;'>&nbsp;</div>"
     else
-      text += "<div class='negative' style='width:#{rating}%;'> &nbsp; </div>"
+      text += "<div class='negative' style='width:#{rating}%;'>&nbsp;</div>"
     end
     text.html_safe
   end
