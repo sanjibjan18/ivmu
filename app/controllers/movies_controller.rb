ed7a41a63ea8e5b3ae12eefb3eeb1c498fe3966a
isiri <isiri@isiri-desktop.(none)>
2011-03-08 09:39:14 +0530

    movie search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,6 +5,9 @@ class MoviesController < ApplicationController
   def index
     @search = Movie.search(params[:search])
     @movies = @search.all.paginate(:page => params[:page] || 1, :per_page => 10)
+    if @movies.size == 1
+      redirect_to movie_path(@movies.first) and return
+    end
   end
 
   def show
