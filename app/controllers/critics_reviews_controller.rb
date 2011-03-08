4db237a0c54c11bb9cf11ed1bbb0595d561f9f07
isiri <isiri@isiri-desktop.(none)>
2011-03-08 18:18:43 +0530

    latest chnages

--- app/controllers/critics_reviews_controller.rb
+++ app/controllers/critics_reviews_controller.rb
@@ -1,4 +1,5 @@
 class CriticsReviewsController < ApplicationController
+    skip_before_filter :authenticate_user!
   layout 'website'
   def show
     @critics_review = CriticsReview.find(params[:id])
