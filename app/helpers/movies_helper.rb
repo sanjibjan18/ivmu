24dc1fe60582177a6f5720adc3cdab03eef6c04b
isiri <isiri@isiri-desktop.(none)>
2011-03-14 18:22:49 +0530

    post comment button and profile link

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,8 +3,8 @@ module MoviesHelper
   def no_of_pepople_liked(movie)
     if current_user
       link_to ((movie.fb_friends_liked(current_user).count rescue 0).to_s +  ' friends liked it!'), '#'
-    #else
-      # '0 friends liked it!'
+    else
+       '0 friends liked it!'
     end
   end
 end
