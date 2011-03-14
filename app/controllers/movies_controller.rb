d206da4fd9edecc21522939fc797147eefb2fb50
isiri <isiri@isiri-desktop.(none)>
2011-03-14 12:50:07 +0530

    foriegn key to primary key in crictics review

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,7 @@ class MoviesController < ApplicationController
 
   def show
     @movie = Movie.find_using_id(params[:id]).includes([:comments, :critics_reviews]).first
-    @critics_review_search = CriticsReview.search({:movie_id_eq => @movie.id, :meta_sort => "review_date.desc"})
+    @critics_review_search = @movie.critics_reviews.search({:meta_sort => "review_date.desc"})
     @critics_reviews = @critics_review_search.all.paginate(:per_page => 2)
     @tweet_search = Tweet.search({:movie_id_eq => @movie.id, :meta_sort => "tweeted_on.desc"})
     @movie_tweets = @tweet_search.all.paginate(:per_page => 4)
