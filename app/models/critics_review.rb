157588a86d805b4aff30c1c08475907e316e883a
isiri <isiri@isiri-desktop.(none)>
2011-03-14 13:03:37 +0530

    foriegn key to primary key in crictics review

--- app/models/critics_review.rb
+++ app/models/critics_review.rb
@@ -1,5 +1,5 @@
 class CriticsReview < ActiveRecord::Base
-  belongs_to :movie, :foreign_key => :movie_name
+  belongs_to :movie, :foreign_key => "movie_name"
   belongs_to :critic_reviewer, :foreign_key => :film_critic_name
 
   scope :for_movie, lambda{|movie| where(:movie_id => movie.id)}
