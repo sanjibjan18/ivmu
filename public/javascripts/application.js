ab0e09e1de43d9fc2e4a67914ec0bce4089fb3cd
isiri <isiri@isiri-desktop.(none)>
2011-03-23 14:43:01 +0530

    application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
  $(document).ready(function($) {
     $('a[rel*=facebox]').facebox();
     $( "#reviews" ).tabs();
-    $("#search_name_contains").autocomplete({source: '/autocomplete', minLength: 3});
+    $("#q").autocomplete({source: '/autocomplete', minLength: 3});
     $("#pagination .muvi_pagination a").live("click", function() {
        $.getScript(this.href); return false;
      });
