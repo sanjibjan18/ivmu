79c8c2f4f534e6888bafcc3294ae8da57bbd1381
isiri <isiri@isiri-desktop.(none)>
2011-03-24 18:07:11 +0530

    latest chnages

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,18 +7,14 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     build_resource
     if resource.save
-      puts "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
       if resource.active?
-        puts "tttttttttttttttttttttttttttttttttttt"
         #redirect_to edit_member_path(resource)
       else
-        puts "ooooooooooooooooooooooooooooooooo"
         #set_flash_message :notice, :inactive_signed_up, :reason => resource.inactive_message.to_s
         expire_session_data_after_sign_in!
         #redirect_to after_inactive_sign_up_path_for(resource)
       end
     else
-      puts "zzzzzzzzzzzzzzzzzzzzzzzzzzzz"
       clean_up_passwords(resource)
       render_with_scope :new
     end
