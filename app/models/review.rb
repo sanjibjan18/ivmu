fec9ad2e78bfff85804dc043cd1f3c8c76914b50
isiri <isiri@isiri-desktop.(none)>
2011-04-05 15:11:47 +0530

    post to wall

--- app/models/review.rb
+++ app/models/review.rb
@@ -13,7 +13,7 @@ class Review < ActiveRecord::Base
         begin
           client = Mogli::Client.new(self.user.facebook_token)
           @myself  = Mogli::User.find("me", client)
-          post = Mogli::Post.new(:message => self.description)
+          post = Mogli::Post.new({:message => self.description, :name => "#{self.user.display_name} rated #{self.movie.name} #{self.rating} starts",:link => "http://ec2-122-248-194-131.ap-southeast-1.compute.amazonaws.com/movies/#{self.movie.permalink}",:caption => '&nbsp;', :description => '&nbsp;', :picture => "http://ec2-122-248-194-131.ap-southeast-1.compute.amazonaws.com/"+ self.movie.banner_image.to_s, :actions => {"name" =>  "See Muvi.in Review", "link" => "http://ec2-122-248-194-131.ap-southeast-1.compute.amazonaws.com/movies/#{self.movie.permalink}"} })
           @myself.feed_create(post)
         rescue Mogli::Client::OAuthException
           # getting this strange exception. (#506) Duplicate status message
@@ -39,5 +39,15 @@ class Review < ActiveRecord::Base
   end
 
   scope :for_movie, lambda{|movie| where(:movie_id => movie.id)}
+
+
+  def link_detail
+    detail = {}
+    detail[:name] =  "#{self.user.display_name} rated #{self.movie.name} #{self.rating} starts",
+    detail[:link] = "http://ec2-122-248-194-131.ap-southeast-1.compute.amazonaws.com/movies/#{self.movie.permalink}"
+    detail[:description] = ''
+    detail
+
+  end
 end
 
