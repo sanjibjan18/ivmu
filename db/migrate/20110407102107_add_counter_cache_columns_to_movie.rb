8305ec0ba1f25210c0769eab38d315b87ccf27c4
isiri <isiri@isiri-desktop.(none)>
2011-04-07 18:20:22 +0530

    latest chnages

--- db/migrate/20110407102107_add_counter_cache_columns_to_movie.rb
+++ db/migrate/20110407102107_add_counter_cache_columns_to_movie.rb
@@ -7,10 +7,10 @@ class AddCounterCacheColumnsToMovie < ActiveRecord::Migration
     add_column :films, :facebook_feeds_count, :integer, :default => 0
     add_column :films, :movie_casts_count, :integer, :default => 0
     add_column :films, :critics_reviews_count, :integer, :default => 0
-
+    Movie.reset_column_information
     Movie.all.each do |movie|
       hash = {:tweets_count => movie.tweets.count, :facebook_feeds_count => movie.facebook_feeds.count, :movie_casts_count => movie.movie_casts.count, :critics_reviews_count => movie.critics_reviews.count}
-      movie.update_attributes(hash)
+      Movie.update_counters movie.id, hash
     end
   end
 
