ad0123c1635a39e91cfcf116f0877d80ddfde855
isiri <isiri@isiri-desktop.(none)>
2011-04-12 11:07:54 +0530

    movie pagination

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -52,7 +52,7 @@ class Movie < ActiveRecord::Base
   scope :latest, order('release_date desc nulls last')
   scope :limit, lambda{|l| limit(limit) }
   scope :name_is_not_blank, where("name IS NOT NULL")
-  scope :released, where("release_date <= ? ", Date.today)
+  scope :released, where("release_date <= ? or  release_date IS NULL", Date.today)
   scope :comming_soon_movies, where("release_date > ? ", Date.today)
 
 #  scope :backwards_name, where("id in (?)", Tweet.select('distinct movie_id').collect(&:movie_id))
