6775158eb11f2b30c3d18699363c7efe4c208f61
isiri <isiri@isiri-desktop.(none)>
2011-04-12 17:58:23 +0530

    poster and trailer updated at added

---
+++ db/migrate/20110412121620_add_poster_updated_at_and_trailer_updated_at_movies.rb
@@ -0,0 +1,22 @@
+class AddPosterUpdatedAtAndTrailerUpdatedAtMovies < ActiveRecord::Migration
+  def self.connection
+    Movie.connection
+  end
+
+  def self.up
+  #  add_column :films, :poster_updated_at,   :datetime
+   # add_column :films, :trailer_updated_at,   :datetime
+
+    Movie.all.each do |movie|
+      hash = {}
+      hash[:poster_updated_at] =  movie.updated_at unless movie.poster_file_name.blank?
+      hash[:trailer_updated_at] = movie.updated_at unless movie.trailer_file_name.blank?
+      movie.update_attributes(hash)
+    end
+
+  end
+
+  def self.down
+  end
+end
+
