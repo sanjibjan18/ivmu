4a12fcab196ec9fb3d3095ca778d65ba3e2a3eaa
isiri <isiri@isiri-desktop.(none)>
2011-04-14 17:26:33 +0530

    latest chnages

--- app/models/facebook_feed.rb
+++ app/models/facebook_feed.rb
@@ -1,6 +1,6 @@
 class FacebookFeed < ActiveRecord::Base
   belongs_to :user
-  belongs_to :movie, :counter_cache => true
+  belongs_to :movie#, :counter_cache => true
 
   scope :like_name, lambda{|name| where("value like ? ", "#{name}%") }
   scope :by_fb_item_id, lambda{|item_id| where(:fb_item_id => item_id) }
