326e557117f60bc18682de58f5f3ff313f659b54
vijendra <vijendrakarkala@gmail.com>
2011-04-25 21:21:30 +0530

    new tweet

--- app/controllers/admin/movie_tweets_controller.rb
+++ app/controllers/admin/movie_tweets_controller.rb
@@ -11,11 +11,29 @@ class Admin::MovieTweetsController < ApplicationController
   def show
     @tweet = @movie.tweets.find(params[:id])
   end
-
+  
+  def new
+    @tweet = @movie.tweets.new
+  end
+  
   def edit
     @tweet = @movie.tweets.find(params[:id])
   end
+  
+  def create
+    @tweet = @movie.tweets.new(params[:tweet])
 
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to(edit_admin_movie_movie_tweet_path(@movie, @tweet), :notice => 'Tweet was successfully created.') }
+        format.xml  { render :xml => @tweet, :status => :created, :location => @tweet }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @tweet.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
 
   def update
     @tweet = @movie.tweets.find(params[:id])
--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -1,5 +1,5 @@
 class ContactUsMailer < ActionMailer::Base
-  default  :from =>  %{"muvi user" <information@muvi.in>},  :content_type => "text/html"
+  default  :from =>  %{"muvi user" <info@muvi.in>},  :content_type => "text/html"
 
   def send_contact_us(object)
     @object = object
