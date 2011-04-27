d1807be35296cab08cedc08c125ca53d250a21ba
isiri <isiri@isiri-desktop.(none)>
2011-04-27 10:09:25 +0530

    review

--- app/models/review.rb
+++ app/models/review.rb
@@ -14,7 +14,7 @@ class Review < ActiveRecord::Base
         begin
           client = Mogli::Client.new(self.user.facebook_token)
           @myself  = Mogli::User.find("me", client)
-          post = Mogli::Post.new({:message => self.description, :name => "#{self.user.display_name} rated #{self.movie.name} #{self.rating} starts",:link => "#{SITE_URL}/movies/#{self.movie.permalink}",:caption => '&nbsp;', :description => "RELEASE DATE: #{self.movie_release_date}", :picture => "#{SITE_URL}/#{self.movie.poster.path(:medium)}", :actions => {"name" =>  "See Muvi.in Review", "link" => "#{SITE_URL}/movies/#{self.movie.permalink}"} })
+          post = Mogli::Post.new({:message => self.description, :name => "#{self.user.display_name} rated #{self.movie.name} #{self.rating} stars",:link => "#{SITE_URL}/movies/#{self.movie.permalink}",:caption => '&nbsp;', :description => "RELEASE DATE: #{self.movie_release_date}", :picture => "#{SITE_URL}/#{self.movie.poster.path(:medium)}", :actions => {"name" =>  "See Muvi.in Review", "link" => "#{SITE_URL}/movies/#{self.movie.permalink}"} })
           @myself.feed_create(post)
         rescue Mogli::Client::OAuthException
           # getting this strange exception. (#506) Duplicate status message
