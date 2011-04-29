b8bdbebf54936bcf85ee716cc26eeeb0ab93eb84
isiri <isiri@isiri-desktop.(none)>
2011-04-29 10:06:56 +0530

    tweet changes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,9 +6,9 @@ class TweetsController < ApplicationController
     @movie ||= Movie.find(params[:id])
     #@tweet_search = Tweet.search({:movie_id_eq => @movie.id, :meta_sort => "tweeted_on.desc"})
     #@movie_tweets = @tweet_search.all.paginate(:page => params[:page], :per_page => 4)
-    my_query = "((select 'movie_review' as model,user_id,description,rating,'' as review,'' as twitter_name,created_at from reviews where reviews.movie_id=#{@movie.id} ) UNION (select 'movie_tweet' as model,user_id,content,rating,review,twitter_screen_name,tweeted_on from tweets WHERE ( tweets.interest = 'f') AND (tweets.review = 'pos' or tweets.review = 'neg' ) AND (tweets.movie_id = #{@movie.id}) ) order by twitter_name asc) as temp"
-    total_entries = ActiveRecord::Base.connection.execute("select count(*) as count from #{my_query}").first['count'].to_i
-    result = ActiveRecord::Base.connection.select_rows("select * from #{my_query}")
+    reviews_query = "((select 'movie_review' as model,user_id,description,rating,'' as review,'' as twitter_name,created_at from reviews where reviews.movie_id=#{@movie.id} ) UNION (select 'movie_tweet' as model,user_id,content,rating,review,twitter_screen_name,tweeted_on from tweets WHERE ( tweets.interest = 'f') AND (tweets.review = 'pos' or tweets.review = 'neg' ) AND (tweets.movie_id = #{@movie.id}) ) order by twitter_name asc) as temp"
+    total_entries = ActiveRecord::Base.connection.execute("select count(*) as count from #{reviews_query}").first['count'].to_i
+    result = ActiveRecord::Base.connection.select_rows("select * from #{reviews_query}")
     @movie_tweets =  result.paginate(:page => params[:page], :per_page =>  4)
 
     respond_to do |format|
