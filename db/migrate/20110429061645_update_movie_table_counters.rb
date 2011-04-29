334464ca88d9ac124d21f8a817f98a6c45f71ebc
isiri <isiri@isiri-desktop.(none)>
2011-04-29 12:02:18 +0530

    update counter

---
+++ db/migrate/20110429061645_update_movie_table_counters.rb
@@ -0,0 +1,18 @@
+class UpdateMovieTableCounters < ActiveRecord::Migration
+  def self.connection
+    Movie.connection
+  end
+  def self.up
+    Movie.reset_column_information
+    Movie.all.each do |movie|
+      hash = {:tweets_count => -movie.tweets_count, :facebook_feeds_count => -movie.facebook_feeds_count, :movie_casts_count => -movie.movie_casts_count, :critics_reviews_count => -movie.critics_reviews_count}
+      Movie.update_counters movie.id, hash
+      hash = nil
+      hash = {:tweets_count => movie.tweets.reviews.pos_or_neg.count, :facebook_feeds_count => movie.facebook_feeds.count, :movie_casts_count => movie.movie_casts.count, :critics_reviews_count => movie.critics_reviews.count}
+      Movie.update_counters movie.id, hash
+    end
+  end
+  def self.down
+  end
+end
+
